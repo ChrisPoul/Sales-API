48fa8c4c5dfb9f868a41edb31e5eac71be812117
ChrisPoul <chrispoulsenfernandez@gmail.com>
2022-01-17 11:30:52 -0600

    Added email field to customer and declared that name is not unique

--- Sales/models/customer/__init__.py
+++ Sales/models/customer/__init__.py
@@ -8,8 +8,9 @@ from . import db, Model
 
 class Customer(db.Model, Model):
     id = Column(Integer, primary_key=True)
-    name = Column(String(200), nullable=False)
+    name = Column(String(200), nullable=False, unique=False)
     phone = Column(String(17), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=False, default="")
     time_created = Column(DateTime, server_default=func.now())
     time_updated = Column(DateTime, server_onupdate=func.now())
     receipts = db.relationship(
