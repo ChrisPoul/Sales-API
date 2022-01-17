ac94a9ce2be4f17bb87d23ffd64c95196a7e7c8f
ChrisPoul <chrispoulsenfernandez@gmail.com>
2022-01-17 12:10:53 -0600

    Added validation for customer phone with corresponding tests

--- Sales/models/customer/validation.py
+++ Sales/models/customer/validation.py
@@ -7,5 +7,12 @@ class CustomerValidation:
     def validate_name(self):
         customer_name = self.customer.name.replace(" ", "")
         if customer_name.isalpha() is False:
-            self.error = "El nombre solo puede contener letras"
+            self.error = "El nombre solo puede contener letras y espacios"
+        return self.error
+
+    def validate_phone(self):
+        customer_phone = self.customer.phone.replace(" ", "")
+        if customer_phone.isnumeric() is False:
+            self.error = "El número de teléfono solo puede contener números y espacios"
+
         return self.error
--- tests/customer_tests/test_validate.py
+++ tests/customer_tests/test_validate.py
@@ -20,3 +20,15 @@ class TestValidateName(CustomerTest):
         error = self.customer.validation.validate_name()
 
         self.assertNotEqual(error, None)
+
+
+class ValidatePhone(CustomerTest):
+
+    def test_should_not_return_error_given_valid_phone(self):
+        self.customer.phone = "222 123 4567"
+        error = self.customer.validation.validate_phone()
+
+        self.assertEqual(error, None)
+
+    def test_should_return_error_given_non_numerical_characters_in_phone(self):
+        self.customer.phone = "invalid phone"
